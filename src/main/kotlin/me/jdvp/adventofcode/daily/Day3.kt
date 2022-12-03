46d3410b505ed8ce6fc8de247b9b1565ab25fda2
JD Porterfield <jdrocks94@gmail.com>
2022-12-02 22:45:25 -0800

    Replace iteration with chunked
    feat : This will be less error prone since it doesn't
    require thinking about which indexes are correct etc.

--- src/main/kotlin/me/jdvp/adventofcode/daily/Day3.kt
+++ src/main/kotlin/me/jdvp/adventofcode/daily/Day3.kt
@@ -8,24 +8,6 @@ object Day3 {
         return getResourceAsText("Day3Input").lines()
     }
 
-    private fun intersectAll(vararg charSequence: CharSequence): Char {
-        return charSequence.map { it.toSet() }.reduce { acc, cs ->
-            acc.intersect(cs)
-        }.first()
-    }
-
-    private fun String.getCompartmentMatchingItem(): Char {
-        val compartment1 = subSequence(0, length / 2)
-        val compartment2 = subSequence(length / 2, length)
-        return intersectAll(compartment1, compartment2)
-    }
-
-    private fun List<String>.toMatchingCompartmentItems(): List<Char> {
-        return map { rucksack ->
-            rucksack.getCompartmentMatchingItem()
-        }
-    }
-
     private fun Char.toItemScore(): Int {
         return if (isLowerCase()) {
             1 + code - 'a'.code
@@ -34,12 +16,21 @@ object Day3 {
         }
     }
 
+    private fun Collection<String>.intersectAll(): Char {
+        return map { it.toSet() }.reduce { acc, s ->
+            acc.intersect(s)
+        }.first()
+    }
+
+    private fun List<String>.toMatchingCompartmentItems(): List<Char> {
+        return map { rucksack ->
+            rucksack.chunked(rucksack.length / 2).intersectAll()
+        }
+    }
+
     private fun List<String>.toElfBadges(): List<Char> {
-        return (indices step 3).map { groupIndex ->
-            val elf1 = get(groupIndex)
-            val elf2 = get(groupIndex + 1)
-            val elf3 = get(groupIndex + 2)
-            return@map intersectAll(elf1, elf2, elf3)
+        return chunked(3).map { items ->
+            return@map items.intersectAll()
         }
     }
 
